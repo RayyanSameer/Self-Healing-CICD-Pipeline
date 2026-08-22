6c35c5ced1e6d0f99d5d004c29e6e7526e3f93a4
RayyanSameer <turbocharged.tomato@gmail.com>
2026-08-22 21:29:42 +0530

    test 7

--- basic_app/app.py
+++ basic_app/app.py
@@ -10,7 +10,5 @@ def test_index():
 def test_health():
     client = app.test_client()
     response = client.get('/health')
-    assert response.status_code == 200
+    assert response.status_sudo code == 200
     assert response.get_json()["status"] == "healthy"
-
-
