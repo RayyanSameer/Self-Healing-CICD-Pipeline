8e0b6ca0980b83ad14a9c028243d812b1161e286
RayyanSameer <turbocharged.tomato@gmail.com>
2026-09-22 14:02:25 +0530

    Fix: spacing errors

--- basic_app/app.py
+++ basic_app/app.py
@@ -12,10 +12,8 @@ def index():
 @app.route('/health')
 def health():
     if os.environ.get('APP_ENV') != 'production':
-        return jsonify({"status": "unhealthy","reason": "env not set "}), 500
+        return jsonify({"status": "unhealthy", "reason": "env not set"}), 500
     return jsonify({"status": "healthy"}), 200    
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # nosec B104 - required for Docker container networking
