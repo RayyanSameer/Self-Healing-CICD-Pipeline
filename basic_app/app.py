1d76a8c811d67e1fb7cb792a8edf6d1199471d92
RayyanSameer <turbocharged.tomato@gmail.com>
2026-09-24 16:21:44 +0530

    fix: trail spaces

--- basic_app/app.py
+++ basic_app/app.py
@@ -8,12 +8,11 @@ app = Flask(__name__)
 def index():
     return jsonify({"message": "hello world"})
 
-
 @app.route('/health')
 def health():
     if os.environ.get('APP_ENV') != 'production':
         return jsonify({"status": "unhealthy", "reason": "env not set"}), 500
-    return jsonify({"status": "healthy"}), 200    
+    return jsonify({"status": "healthy"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # nosec B104 - required for Docker container networking
