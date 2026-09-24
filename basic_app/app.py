0a7dc415ecdb15c9bf1bb7e6d350205a546e8065
RayyanSameer <turbocharged.tomato@gmail.com>
2026-09-24 16:24:55 +0530

    fix: spacing

--- basic_app/app.py
+++ basic_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-import os \
+import os 
 
 app = Flask(__name__)
 
@@ -8,11 +8,13 @@ app = Flask(__name__)
 def index():
     return jsonify({"message": "hello world"})
 
+
 @app.route('/health')
 def health():
     if os.environ.get('APP_ENV') != 'production':
         return jsonify({"status": "unhealthy", "reason": "env not set"}), 500
     return jsonify({"status": "healthy"}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # nosec B104 - required for Docker container networking
